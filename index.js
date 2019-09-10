6446c69ab089d8d18dd4ba690311dd8121b1c51a
Andrewfurgiuele <furgiueleandrew1@gmail.com>
2019-09-09 22:56:47 -0400

    bare bones of the site

---
+++ index.js
@@ -0,0 +1,12 @@
+(function (window, document, undefined) {
+
+    // code that should be taken care of right away
+
+    window.onload = init;
+    function init() {
+
+
+
+    }
+})(window, document, undefined);
+
