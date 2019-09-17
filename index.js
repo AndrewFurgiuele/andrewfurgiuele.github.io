3b1ccd583635d8f102c681d74be84b8f48389c41
Andrewfurgiuele <furgiueleandrew1@gmail.com>
2019-09-16 23:21:59 -0400

    icons

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-(function (window, document, undefined) {
+$(document).ready(function(){
 
-    // code that should be taken care of right away
+    $(".link").hover(function(){
+        showLinkLabel(true, this);
+    }, function(){
+        showLinkLabel(false, this);    
+    });
 
-    window.onload = init;
-    function init() {
+  });
+
+  $(this).css("margin-top", "0%");
+  $(this).css("height", "9vw");
+  $(this).css("width", "9vw");
 
 
+function showLinkLabel(show, thus){
 
+    if(show){
+        $(thus).css("margin-top", "0%");
+        $(thus).css("height", "9vw");
+        $(thus).css("width", "9vw");
+      
+    }else{
+        $(thus).css("margin-top", "20%");
+        $(thus).css("height", "5vw");
+        $(thus).css("width", "5vw");
+      
     }
-})(window, document, undefined);
+}
 
