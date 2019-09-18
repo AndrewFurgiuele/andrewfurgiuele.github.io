d2f78cc97940962c90b72222afd54409bd8175a9
Andrewfurgiuele <furgiueleandrew1@gmail.com>
2019-09-17 23:08:30 -0400

    labels

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 $(document).ready(function(){
 
-    $(".link").hover(function(){
-        showLinkLabel(true, this);
+    $(".link-div").hover(function(){
+        showLinkLabel(true, $(this));
     }, function(){
-        showLinkLabel(false, this);    
+        showLinkLabel(false, $(this));    
     });
 
   });
 
-  $(this).css("margin-top", "0%");
-  $(this).css("height", "9vw");
-  $(this).css("width", "9vw");
 
-
-function showLinkLabel(show, thus){
-
+function showLinkLabel(show, div){
     if(show){
-        $(thus).css("margin-top", "0%");
-        $(thus).css("height", "9vw");
-        $(thus).css("width", "9vw");
-      
+        div.children('p').css("opacity", 1);
     }else{
-        $(thus).css("margin-top", "20%");
-        $(thus).css("height", "5vw");
-        $(thus).css("width", "5vw");
-      
+        div.children('p').css("opacity", 0);
     }
 }
 
