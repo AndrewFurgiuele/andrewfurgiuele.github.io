3ab7ddeaf52955d0e9dbe4a15e78ae777ec431b0
Andrewfurgiuele <furgiueleandrew1@gmail.com>
2019-09-19 21:34:08 -0400

    other pages

--- index.js
+++ index.js
@@ -11,9 +11,24 @@ $(document).ready(function(){
 
 function showLinkLabel(show, div){
     if(show){
-        div.children('p').css("opacity", 1);
+        div.children('p').fadeIn(1000);
+        div.children('img').fadeTo(1000, 1);
+        // div.children('img').css('z-index', 10000);
     }else{
-        div.children('p').css("opacity", 0);
+        div.children('p').fadeOut(1000);
+        div.children('img').fadeTo(1000, .75);
     }
 }
 
+
+
+
+
+function copyEmail(){
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($("#email").html()).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+}
